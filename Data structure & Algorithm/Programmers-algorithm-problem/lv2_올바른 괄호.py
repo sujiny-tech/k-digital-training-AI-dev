927d4fa7560030c06bb4ce7516a1a936fa5bd3e3
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2020-12-04 00:28:33 +0900

    Update lv2_올바른 괄호.py

--- Data structure & Algorithm/Programmers-algorithm-problem/lv2_올바른 괄호.py
+++ Data structure & Algorithm/Programmers-algorithm-problem/lv2_올바른 괄호.py
@@ -8,7 +8,7 @@ def solution(s):
             stack.append(s[i]) #(일때 stack에 쌓아주고
         else:
             if not stack:
-                return False #)일때, stack없으면 짝이 안맞으니까 Flase
+                return False #)일때, stack없으면 짝이 안맞으니까 False
             stack.pop() #stack있으면 pop
             
     #stack에 쌓여있는게 없으면 짝이 맞는거, 쌓여있으면 짝 안맞음
