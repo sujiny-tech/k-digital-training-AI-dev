81207f0df128017bb5f0d1eb3d77c7504b8560b8
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2020-12-06 21:50:01 +0900

    Update lv3_FloodFill.py

--- Data structure & Algorithm/Programmers-algorithm-problem/lv3_FloodFill.py
+++ Data structure & Algorithm/Programmers-algorithm-problem/lv3_FloodFill.py
@@ -1,32 +1,35 @@
-def solution(n, m, image):
-    answer = 0
-    # 붙어있는게 몇개냐 > bfs접근법(동서남북)
-    # 예전에 풀었던 얼음얼리기였던거같은데
-    visited = [[1]*m]*n  # 색깔 처리한거
-    def dfs(row, col, color):
-        if row <= 0 or row > n or col <= 0 or col > m or image[row][col]==0:
-            return
+def solution(n,m,image):
+    answer=0
+    
+    def bfs(row, col):
+        Q=[]
+        dx=[-1,0, 1,0]
+        dy=[0,1,0,-1]
 
-        image[row][col]=0
+        Q.append([row, col, image[row][col]]) #추가
+        image[row][col]=0 #방문 처리
 
-        visited[row][col] = 0
-
-        dfs(row, col + 1, color)
-        dfs(row, col - 1, color)
-        dfs(row + 1, col, color)
-        dfs(row - 1, col, color)
-        
-    #방문했는지
+        while Q:
+            row, col, color=Q.pop(0)
+            
+            #동서남북 처리
+            for i in range(4):
+                nx=row+dx[i]
+                ny=col+dy[i]
+                
+                #해당 인덱스에 대해,
+                if 0<=nx and nx<len(image) and 0<=ny and ny<len(image[0]):
+                    #같은 색이면
+                    if image[nx][ny]==color:
+                        image[nx][ny]=0
+                        Q.append([nx, ny, color])
+                        
+    ##
     for i in range(n):
         for j in range(m):
-            if visited[i][j]!=0 :
-                print(image[i][j])
-                dfs(i, j, image[i][j])
-                print("방문:", visited)
-                answer += 1
-    print(visited, image)
+            if image[i][j]!=0:
+                bfs(i, j) #너비에 대해 다 방문처리하고
+                answer+=1 #카운트+1
     return answer
 
 print(solution(3,2,[[1,2],[1,2],[4,5]]))
-
-##ing.....
